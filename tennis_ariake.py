ac01e775bb7af01a6295387c796c6b6c2aa96827
EricSuica <85236075+EricSuica@users.noreply.github.com>
2025-02-07 19:49:02 +0900

    Update tennis_ariake.py

--- tennis_ariake.py
+++ tennis_ariake.py
@@ -30,7 +30,7 @@ options.add_argument("--window-size=1920x1080")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-blink-features=AutomationControlled")  
-driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+webdriver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")  
 import tempfile
 unique_user_data_dir = tempfile.mkdtemp()
